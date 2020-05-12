3dc122fc479bf039cd0f62801abc9b3bb9c813df
Thamalu <thamalumaliththa@gmail.com>
2020-05-12 21:45:07 +0530

    Add support for chart items and panda input

--- modules/G2.py
+++ modules/G2.py
@@ -1,4 +1,5 @@
 from IPython.display import Javascript, display_html
+import pandas as pd
 
 
 # Coded by Thamalu Maliththa Piyadigama
@@ -51,36 +52,124 @@ def run_code(notebook_code):
 display_html(loading_code,raw=True)
 
 
+class ChartItem:
+    
+        
+    def __init__(self, name):
+        
+        aes = ['position','size','color','shape','label','adjust']
+        
+        self.item_code = 'chart.'
+        self.__name = name
+        
+        for i in aes:
+            self.__dict__[i]= self.get_aes_function(i)
+        
+        
+    def add_code(self):
+        self.item_code += self.__name + '(%s)'
+        
+        
+    def attach_config(self,config_dict):
+        if config_dict:
+            self.item_code = self.item_code%config_dict
+        else:
+            self.item_code = self.item_code%''
+                   
+    def __str__(self):
+        return self.item_code
+    
+    def get_aes_function(self,aes):
+        def aestatic(map,**config):
+            if config:
+                self.item_code += "."+aes+"('%s',%s)"%(map,config)
+            else:
+                self.item_code += "."+aes+"('%s')"%map
+            return self
+        return aestatic
+
+    
+    
+#     def position(self,map):
+#         self.item_code += ".position('%s')"%map
+#         return self
+        
+
+
 
 
 # Chart class to be developed
 class Chart:
     # Chart object contains all properties and behaviours of a class
     
-    def __init__(self, **config_dict):
+    #list of chart geom functions
+    
+    
+    def __init__(self, height=500, width=400, **config):
         
+        geom = ['point', 'path', 'edge', 'heatmap', 'interval', 'polygon', 'schema', 'line', 'area']
         
-        self.layout = config_dict
+        self.layout = config
         self.layout['container'] = 'element.get(0)'
+        self.layout['height'] = height
+        self.layout['width'] = width
+        self.additional_code = ''
+        self.chart_items = []
         
+        for i in geom:
+            self.__dict__[i]= self.get_geom_function(i)
+        
+        
+
+            
     def data(self,data):
+        if type(data) == list:
+            self.chart_data = data
+        elif type(data) == pd.core.frame.DataFrame:
+            self.chart_data = data.to_dict(orient='record')
+        else:
+            raise TypeError("Unsupported input data type. Only `pandas.DataFrame` and `list` of `dictionary`s are supported.")
         
-        self.data = data
+    def get_layout_code(self):
+        return ('var chart = new G2.Chart(%s);\n'%self.layout).replace('\'element.get(0)\'','element.get(0)')
+    
+    def add_code(self,js_code):
+        self.additional_code += js_code+';'
         
+    def create_item_code(self):
+        
+        item_code=''
+        for i in self.chart_items:
+            item_code+=str(i)+';\n'
+            
+        return item_code
         
     def render(self):
         # Should be modified in a better form
-        layout_code = ('var chart = new G2.Chart(%s);'%self.layout).replace('\'element.get(0)\'','element.get(0)')
-        data_code = 'var data = %s; \n chart.data(data);'%self.data
-        item_code = 'chart.interval().position(\'x*y\');'
+        layout_code = self.get_layout_code()
+        data_code = 'var data = %s; \n chart.data(data);'%self.chart_data
+        #item_code = 'chart.interval().position(\'x*y\');'
+        item_code = self.create_item_code()
         element_code = ''
+        additional_code = self.additional_code
         render_code = 'chart.render();'
         
         
         
-        final_code = layout_code+data_code+item_code+element_code+render_code
-        return Javascript(get_notebook_code(final_code))
         
+        final_code = layout_code+data_code+item_code+element_code+additional_code+render_code
+        return Javascript(get_notebook_code(final_code))
+    
+    def get_geom_function(self, geom):
+        
+        def geometry(**config):
+            chart_item = ChartItem(geom)
+            chart_item.add_code()
+            chart_item.attach_config(config)
+            self.chart_items.append(chart_item)
+            return chart_item
+        return geometry
+    
    
 
 
