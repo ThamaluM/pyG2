9c847330023fb1e6f9ec484e932af9dde37e3d73
Thamalu <thamalumaliththa@gmail.com>
2020-05-13 21:33:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setuptools.setup(
 
     name = "pyG2",
     version = "0.0alpha",
+    author = "Thamalu Maliththa Piyadigama",
     long_description = Path("README.md").read_text(),
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ThamaluM/pyG2",
     packages = setuptools.find_packages(exclude=['test','.git'])
 
 )
