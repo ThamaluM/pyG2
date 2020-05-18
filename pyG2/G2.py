e1be7f478ef8dc3994b79428fa421bf6a39d2085
Thamalu <thamalumaliththa@gmail.com>
2020-05-18 18:22:23 +0530

    Fixed G2 Loading and add clear functions

--- pyG2/G2.py
+++ pyG2/G2.py
@@ -5,6 +5,7 @@ import pkgutil
 
 # Coded by Thamalu Maliththa Piyadigama
 
+#CDN loading (not used currently)
 loading_code = '''
 <script type="text/javascript">
 require.config({
@@ -24,8 +25,10 @@ require(['G2'], function(G2) {{
 
 '''
 
+# offline loading
 loading_code_1 ='''<script type="text/javascript">
-              
+           
+        require.undef("G2");   
         define('G2', function(require, exports, module) {
             %s
         });
@@ -67,9 +70,20 @@ display_html(loading_code_1%g2_source,raw=True)
 
 
 class ChartItem:
+
+    ''' 
+        An object contains codes related to an geometric item.
+    '''
     
         
     def __init__(self, name):
+
+        '''
+        parameters
+        ===========
+        name : string
+        
+        '''
         
         aes = ['position','size','color','shape','label','adjust']
         
@@ -81,10 +95,12 @@ class ChartItem:
         
         
     def add_code(self):
+        ''' Prepare basic format of the code snippet '''
         self.item_code += self.__name + '(%s)'
         
         
     def attach_config(self,config_dict):
+        ''' Attach geometry item configuration if such configuration exists. '''
         if config_dict:
             self.item_code = self.item_code%config_dict
         else:
@@ -94,7 +110,29 @@ class ChartItem:
         return self.item_code
     
     def get_aes_function(self,aes):
+        ''' 
+        Attach corresponding aesthetic function
+
+        Parameters
+        ==========
+        aes: string, name of the aesthatic function
+
+        Return
+        ======
+        aesthetic function of that name
+        '''
         def aestatic(map,*values,**config):
+
+            ''' 
+            An aesthetic function to generate aesthetic
+
+            Parameters
+            ==========
+            map: string, the variable map of the asesthetic e.g 'x*y'
+            values: xargs [optional], values of that aesthetic 
+            config: xxargs, configuration details
+
+            '''
             if config:
                 self.item_code += "."+aes+"('%s',%s)"%(map,config)
             else:
@@ -113,9 +151,15 @@ class ChartItem:
         
 
 class CoordinateControl:
+
+    ''' Object contain coordinate controlling code snippets '''
     
     def __init__(self,name):
-        
+        '''
+        Parameters:
+        ==========
+        name: string, name of the coordinate system - rect/cartesian,polar,theta,helix
+        '''
         coord_func = ['reflect','rotate', 'scale','transpose']
         
         self.item_code = 'chart.'
@@ -125,6 +169,17 @@ class CoordinateControl:
             self.__dict__[i]= self.get_coord_function(i)
             
     def get_coord_function(self,name):
+        ''' 
+        Get coordinate system transformation functions
+
+        Parameters
+        ==========
+        name: string, name of the coordinate transformation
+
+        return
+        =======
+        function
+        '''
         def coord_function(*amount):
             
             if name == 'reflect':
@@ -150,6 +205,7 @@ class CoordinateControl:
         
         
     def add_code(self):
+        ''' Prepare basic code format '''
         self.item_code += 'coordinate(\''+self.__name+'\')'
         
         
@@ -166,6 +222,8 @@ class CoordinateControl:
  
 # Objects to contain codes related to annotations
 class AnnotationObject:
+
+    ''' Object that contains an annotation related code snippet '''
     
     #annots = ['arc','dataMarker','dataRegion','image','layout','line','region','regionFilter','render','text']
     code = 'chart.annotation().'
@@ -181,6 +239,16 @@ class AnnotationObject:
     
     
     def get_annot_function(self,name):
+        ''' Gives an function for annotation type
+        Parameters
+        =========
+        name: string, name of the function
+
+        return 
+        =======
+        function
+
+        '''
         def annot_function(**config):
             self.code += name+'(%s)'%config
             return self
@@ -197,9 +265,20 @@ class Chart:
     # Chart object contains all properties and behaviours of a class
     
     #list of chart geom functions
+    '''
+    Contains all details of a chart
+    '''
     
     
     def __init__(self, height=500, width=400, **config):
+
+        '''
+        Parameters
+        ===========
+        height = int [optional], height of the chart in ppt
+        width  = int [optional], width of the chart in ppt
+        config = xxargs [optional], other configurations
+        '''
         
         geom = ['point', 'path', 'edge', 'heatmap', 'interval', 'polygon', 'schema', 'line', 'area']
         
@@ -225,6 +304,13 @@ class Chart:
 
             
     def data(self,data):
+        '''
+        Provide data to the graph
+
+        Parameters
+        ==========
+        data: list of dictionaries/ pandas.DataFrame object (recommended)
+        '''
         if type(data) == list:
             self.chart_data = data
         elif type(data) == pd.core.frame.DataFrame:
@@ -233,40 +319,77 @@ class Chart:
             raise TypeError("Unsupported input data type. Only `pandas.DataFrame` and `list` of `dictionary`s are supported.")
         
     def get_layout_code(self):
+        ''' code for basic layout '''
         return ('var chart = new G2.Chart(%s);\n'%self.layout).replace('\'element.get(0)\'','element.get(0)')
     
     def add_code(self,js_code):
+        ''' add an arbitratry javascript code to the javascript code body '''
         self.additional_code += js_code+';'
         
     def coordinate(self,name,**config):
+        '''
+        configure coordinate system
+
+        Parameters
+        ==========
+        name : string, name of the coordinate system - rect, polar, theta, helix
+        configuration: xxargs, coordinate system configurations
+
+        return
+        =======
+        string
+        '''
         self.coordinate_sys = CoordinateControl(name)
         self.coordinate_sys.add_code()
         return self.coordinate_sys
     
     def scale(self,variable,**config):
+        '''
+        Configure scale
+
+        Parameters
+        ==========
+        variable: string, corresponding variable
+        config: xxargs, configurations for the coordinate system
+
+        Return 
+        =======
+        Chart
+
+        '''
         code = "chart.scale('%s',%s)"%(variable,config)
         self.scale_details.append(code)
         return self
     
     def axis(self,variable,**config):
+        ''' 
+        Axis configuration
+
+        Parameters
+        ==========
+        variable: string, variable related to the axis
+        config: xxargs [optional] , axis configurations
+        '''
         code = "chart.axis('%s',%s)"%(variable,config)
         self.scale_details.append(code)
         return self
     
     def create_scale_code(self):
+        '''  Generate code snippet for coordinate configuration'''
         code = ""
         for i in self.scale_details:
             code += i + ';\n'
         return code
     
     def create_axis_code(self):
+        ''' Generate code snippet for axis configuration '''
         code = ""
         for i in self.axis_details:
             code += i + ';\n'
         return code
         
     def create_item_code(self):
-        
+        ''' Generate code snippet for geomtry objects '''
         item_code=''
         for i in self.chart_items:
             item_code+=str(i)+';\n'
@@ -274,7 +397,7 @@ class Chart:
         return item_code
     
     def create_annotation_code(self):
-        
+        ''' Generate code snippet for annotations '''
         item_code=''
         for i in self.annotate_items:
             item_code+=str(i)+';\n'
@@ -282,9 +405,27 @@ class Chart:
         return item_code
     
     def tooltip(self,**config):
-         self.tooltip_details = "chart.tooltip(%s)"%config
-            
+        ''' 
+        Configure tooltip 
+        
+        Parameters
+        ==========
+        config: xxargs, configurations of tooltip
+        '''
+
+        self.tooltip_details = "chart.tooltip(%s)"%config
+
+    # To do: have to consider about multiple legend items      
     def legend(self,value='',**config):
+
+        ''' 
+        Configure legend 
+        
+        Parameters
+        ==========
+        value: string [optional], variable of the corresponding leggend 
+        config: xxargs, configurations of legends
+        '''
         if value and config:
             self.legend_details = "chart.legend('%s',%s)"%(value,config)
         elif value:
@@ -293,20 +434,47 @@ class Chart:
             self.legend_details = "chart.legend(%s)"%config
             
     def annotation(self):
-        
+        ''' 
+        Add annotaion objects
+        '''
         annot_obj = AnnotationObject()
         self.annotate_items.append(annot_obj)
         return annot_obj
     
     def create_annotate_code(self):
-        
+        '''
+        Create annotation code snippets from the annotation objects
+        '''
         item_code=''
         for i in self.annotate_items:
             item_code+=str(i)+';\n'
             
         return item_code
+
+    def clear(self):
+        ''' Clear all chart configurations except layout '''
+        self.additional_code = ''
+        self.chart_items = []
+        self.annotate_items = []
+        
+        self.coordinate_sys = ''
+        self.scale_details = []
+        self.axis_details = []
+        self.tooltip_details = ''
+        self.legend_details = ''
+        
+
+    def clear_items(self):
+        ''' Clear geometry details '''
+        self.chart_items = []
+
+    def clear_annotations(self):
+        ''' Clear annotation details '''
+        self.annotate_items = []
         
     def render(self):
+        ''' Render the chart '''
+
         # Should be modified in a better form
         layout_code = self.get_layout_code()
         data_code = 'var data = %s; \n chart.data(data);'%self.chart_data
@@ -329,7 +497,13 @@ class Chart:
         return Javascript(get_notebook_code(final_code))
     
     def facet_render(self, shape, **config):
-        
+        ''' Render chart in facets
+        Parameters
+        ===========
+        shape = string , shape of the facets - rect, list , matrix , tree , circle
+        config = xxargs , configurations - fields, padding
+        '''
+
         # Should be modified in a better form
         layout_code = self.get_layout_code()
         data_code = 'var data = %s; \n chart.data(data);'%self.chart_data + '\n'
@@ -340,7 +514,7 @@ class Chart:
         coordinate_code = str(self.coordinate_sys) + ';\n'
         tooltip_code = self.tooltip_details+';\n'
         legend_code = self.legend_details + ';\n'
-        annotate_code = self.create_annotate_code()
+        annotate_code = ''#self.create_annotate_code()   # Have to study how annotations are handled in facets
         element_code = ''
         additional_code = self.additional_code
         render_code = 'chart.render();'
@@ -360,7 +534,17 @@ class Chart:
         
     
     def get_geom_function(self, geom):
+        ''' 
+        Get geometry function
         
+        Parameters
+        ==========
+        geom: string, name of the geometry point, line, interval, area, polygon, path, heatmap, schema
+
+        Return 
+        =====
+        Function
+        '''
         def geometry(**config):
             chart_item = ChartItem(geom)
             chart_item.add_code()
