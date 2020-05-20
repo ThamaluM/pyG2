cdddbf367c86bf0851135899054890e7b0a9b4b9
Thamalu <thamalumaliththa@gmail.com>
2020-05-20 13:19:23 +0530

    Specify package data

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     url = "https://github.com/ThamaluM/pyG2",
     packages = setuptools.find_packages(exclude=['test','.git','examples','docs']),
     install_requires=["ipython", "pandas"],
+    package_data = {"pyG2":["*.js"]},
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python ",
